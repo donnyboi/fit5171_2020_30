ac419edf0990e18f7905d13219b4f06226ed98f8
donnyboi <diresher@gmail.com>
2020-04-17 16:27:02 +1000

    second commit

---
+++ src/test/java/allaboutecm/model/MusicalInstrumentUnitTest.java
@@ -0,0 +1,13 @@
+package allaboutecm.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.net.URL;
+
+public class MusicalInstrumentUnitTest {
+
+}
--- src/test/java/allaboutecm/model/MusicianUnitTest.java
+++ src/test/java/allaboutecm/model/MusicianUnitTest.java
@@ -1,12 +1,15 @@
 package allaboutecm.model;
 
+import com.sun.xml.internal.xsom.impl.util.Uri;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.MalformedInputException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,7 +20,15 @@ public class MusicianUnitTest {
     @BeforeEach
     public void setup() { musician = new Musician("Don Bopearachchi"); }
 
-
+    @Test
+    @DisplayName("More than two words")
+    public void musicianNameHasMoreThanTwoWord() {
+        String word = musician.getName().trim();
+        int count;
+        if (word == null) { count = 0; }
+        count = word.isEmpty() ? 0 : word.split("\\s+").length;
+        assertTrue(count > 1);
+    }
 
     @Test
     @DisplayName("Album name cannot be null")
@@ -36,6 +47,12 @@ public class MusicianUnitTest {
         musician.setName(musicianName);
         Musician anotherMusician = new Musician(musicianName);
         assertTrue(musician.equals(anotherMusician));
+    }
+
+    @Test
+    @DisplayName("Check if URL has a protocl")
+    public void displayTrueForLinksWithoutProtocol() {
+        assertThrows(MalformedURLException.class, () -> musician.setMusicianUrl(new URL("www.google.com/")));
     }
 
 }
